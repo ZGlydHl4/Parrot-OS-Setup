3989dff3fef01ca78c9a875ba70ef406c747e669
dirtyx <alexislecuyer14@gmail.com>
2022-02-04 15:52:38 +0100

    config

--- setup.sh
+++ setup.sh
@@ -47,3 +47,4 @@ wget -c https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"' $HOME/.zshrc
+mkdir -p $HOME/.config/terminator
