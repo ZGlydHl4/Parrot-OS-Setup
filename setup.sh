0474e53f76ec121679af1484a0374d5eeeba1ce0
dirtyx <alexislecuyer14@gmail.com>
2022-06-19 18:15:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
+#! /bin/bash
+
 apt-get remove --autoremove burpsuite -y \
     && apt-get purge burpsuite -y
 apt-get autoremove -y
 apt-get update -y
 apt-get full-upgrade -y
-apt-get install wpscan sshuttle open-vm-tools open-vm-tools-desktop gcc-multilib -y
+apt-get install open-vm-tools open-vm-tools-desktop zsh zsh-autosuggestions -y
 systemctl start postgresql \
     && msfdb init
-wget "https://portswigger-cdn.net/burp/releases/download?product=community&version=2021.12.1&type=Linux" -O /home/parrot/Downloads/burpsuite.sh \
+wget "https://portswigger-cdn.net/burp/releases/download?product=community&version=2022.5.1&type=Linux" -O /home/parrot/Downloads/burpsuite.sh \
     && chmod +x /home/parrot/Downloads/burpsuite.sh \
     && /home/parrot/Downloads/burpsuite.sh
 gzip -d /usr/share/wordlists/rockyou.txt.gz
-wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O /usr/share/wordlists/SecList.zip \
-    && unzip /usr/share/wordlists/SecList.zip -y \
+wget -c https://github.com/danielmiessler/SecLists/archive/refs/tags/2022.2.zip -O /usr/share/wordlists/SecList.zip \
+    && unzip /usr/share/wordlists/SecList.zip -d /usr/share/wordlists/ \
+    && mv /usr/share/wordlists/SecLists-2022.2 /usr/share/wordlists/SecLists \
     && rm -f /usr/share/wordlists/SecList.zip
-wget -c https://github.com/NationalSecurityAgency/ghidra/releases/latest/download/ghidra_10.1.2_PUBLIC_20220125.zip -O /usr/share/ghidra_10.1.2_PUBLIC_20220125.zip \
-    && unzip /usr/share/ghidra_10.1.2_PUBLIC_20220125.zip \
-    && rm -f /usr/share/ghidra_10.1.2_PUBLIC_20220125.zip
+wget -c https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.1.4_build/ghidra_10.1.4_PUBLIC_20220519.zip -O /usr/share/ghidra_10.1.4_PUBLIC_20220519.zip \
+    && unzip /usr/share/ghidra_10.1.4_PUBLIC_20220519.zip -d /usr/share/ghidra_10.1.4_PUBLIC_20220519 \
+    && rm -f /usr/share/ghidra_10.1.4_PUBLIC_20220519.zip
 wget -c https://out7.hex-rays.com/files/idafree77_linux.run -O /home/parrot/Downloads/idafree77_linux.run
 chmod +x /home/parrot/Downloads/idafree77_linux.run
 /home/parrot/Downloads/idafree77_linux.run
@@ -24,12 +27,13 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
     && echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list \
     && apt-get update -y \
     && apt-get install sublime-text -y
-apt-get remove --autoremove -y gdb \
-    && apt-get purge -y gdb \
+apt-get autoremove --purge -y gdb \
     && rm -rf /usr/share/gdb \
-    && git clone https://github.com/pwndbg/pwndbg /home/parrot/pwngdb \
-    && sudo -H -u parrot /home/parrot/pwngdb/setup.sh
+    && git clone https://github.com/pwndbg/pwndbg /home/parrot/pwndbg \
+    && chown -R parrot:parrot /home/parrot/pwndbg \
+    && sudo -H -u parrot /home/parrot/pwndbg/setup.sh
 mkdir /home/parrot/binaries
+chown -R parrot:parrot /home/parrot/binaries
 wget -c https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64 -O /home/parrot/binaries/pspy64 \
     && chmod +x /home/parrot/binaries/pspy64
 wget -c https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas_linux_amd64 -O /home/parrot/binaries/linpeas \
@@ -59,6 +63,7 @@ wget -c https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /home/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/parrot/.oh-my-zsh/custom}/themes/powerlevel10k \
     && sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/parrot/.zshrc \
     && wget -c https://raw.githubusercontent.com/ZGlydHl4/Parrot-OS-Setup/master/.p10k.zsh -O /home/parrot/.p10k.zsh \
+    && chown -R parrot:parrot /home/parrot/.p10k.zsh \
     && rm /home/parrot/.zshrc \
-    && wget -c https://raw.githubusercontent.com/ZGlydHl4/Parrot-OS-Setup/master/.zshrc -O /home/parrot/.zshrc
-reboot
+    && wget -c https://raw.githubusercontent.com/ZGlydHl4/Parrot-OS-Setup/master/.zshrc -O /home/parrot/.zshrc \
+    && chown -R parrot:parrot /home/parrot/.zshrc
