8235c2d08a07060521e2808824a47958b97801b3
Alexis <alexislecuyer14@gmail.com>
2022-08-04 13:03:42 +0200

    fdf

--- setup.sh
+++ setup.sh
@@ -75,57 +75,57 @@ git clone https://github.com/pwndbg/pwndbg /home/parrot/pwndbg > /dev/null 2>&1
 echo "[19/34] Downloading CrackMapExec"
 git clone https://github.com/Porchetta-Industries/CrackMapExec /home/parrot/CrackMapExec > /dev/null 2>&1
 
-echo "[20/34] Downloading powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/parrot/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null 2>&1
-
-echo "[21/34] Downloading BurpSuite"
+echo "[20/34] Downloading BurpSuite"
 wget -q "https://portswigger-cdn.net/burp/releases/download?type=Linux" -O /tmp/tmp_downloads/burpsuite.sh
 
-echo "[22/34] Preparing for installation"
+echo "[21/34] Preparing for installation"
 chown -R parrot:parrot /tmp/tmp_downloads
 
-echo "[23/34] Installing rockyou"
+echo "[22/34] Installing rockyou"
 gzip -d /usr/share/wordlists/rockyou.txt.gz
 
-echo "[24/34] Installing SecLists"
+echo "[23/34] Installing SecLists"
 mkdir -p /opt/wordlists/SecLists \
   && tar xf /tmp/tmp_downloads/SecLists.tar.gz -C /opt/wordlists/SecLists --strip-components 1
 
-echo "[25/34] Installing Ghidra"
+echo "[24/34] Installing Ghidra"
 unzip /tmp/tmp_downloads/ghidra.zip -d /opt/ > /dev/null 2>&1 \
   && mv /opt/ghidra_* /opt/Ghidra \
   && alias ghidra=/opt/Ghidra/ghidraRun
 
-echo "[26/34] Installing chisel"
+echo "[25/34] Installing chisel"
 gzip -d /tmp/tmp_downloads/chisel_linux.gz \
   && mv /tmp/tmp_downloads/chisel_linux /opt/binaries/chisel_linux
 
 gzip -d /tmp/tmp_downloads/chisel_windows.gz \
   && mv /tmp/tmp_downloads/chisel_windows /opt/binaries/chisel_windows
 
-echo "[27/34] Installing sublime-text"
+echo "[26/34] Installing sublime-text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add - > /dev/null 2>&1 \
   && apt-get -qq install apt-transport-https -y > /dev/null 2>&1 \
   && echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list \
   && apt-get -qq update -y > /dev/null 2>&1 \
   && apt-get -qq install sublime-text -y > /dev/null 2>&1
 
-echo "[28/34] Installing vscode"
+echo "[27/34] Installing vscode"
 dpkg -i /tmp/tmp_downloads/vscode.deb > /dev/null 2&>1
 
-echo "[29/34] Installing CrackMapExec"
+echo "[28/34] Installing CrackMapExec"
 cd /home/parrot/CrackMapExec; poetry install > /dev/null 2&>1; poetry run crackmapexec > /dev/null 2&>1
 
-echo "[30/34] Installing pwndbg"
+echo "[29/34] Installing pwndbg"
 apt-get -qq autoremove --purge -y gdb > /dev/null 2&>1 \
   && rm -rf /usr/share/gdb \
   && chown -R parrot:parrot /home/parrot/pwndbg \
   && sudo -H -u parrot /home/parrot/pwndbg/setup.sh > /dev/null 2&>1
 
-echo "[31/34] Installing oh-my-zsh"
+echo "[30/34] Installing oh-my-zsh"
 chmod +x /tmp/tmp_downloads/oh-my-zsh_install.sh \
   && sed -i 's/RUNZSH=${RUNZSH:-yes}/RUNZSH=${RUNZSH:-no}/g' /tmp/tmp_downloads/oh-my-zsh_install.sh \
   && sudo -H -u parrot /tmp/tmp_downloads/oh-my-zsh_install.sh
+
+echo "[31/34] Downloading powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/parrot/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null 2>&1
 
 echo "[32/34] Installing powerlevel10k"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/parrot/.zshrc \
