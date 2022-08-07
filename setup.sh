55a2c0249c99a71d0fc347e5dfb6a1ac60cbc5c8
Alexis <alexislecuyer14@gmail.com>
2022-08-07 15:36:53 +0200

    fdf

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 echo "[1/34] Initialization"
-apt-get -qq update > /dev/null 2>&1
-apt-get -qq full-upgrade -y > /dev/null 2>&1
-apt-get -qq autoremove --purge -y > /dev/null 2>&1
-apt-get -qq autoremove --purge -y burpsuite > /dev/null 2>&1
+apt-get update 
+apt-get full-upgrade -y 
+apt-get autoremove --purge -y 
+apt-get autoremove --purge -y burpsuite 
 mkdir -p /tmp/tmp_downloads/
 mkdir -p /opt/binaries/
 mkdir -p /home/parrot/.local/share/fonts/
 mkdir -p /home/parrot/.config/terminator
-systemctl --quiet start postgresql && msfdb init > /dev/null 2>&1
+systemctl --quiet start postgresql && msfdb init 
 
 echo "[2/34] Searching for latest releases"
 ghidra_url=$(curl -Ls "https://github.com/NationalSecurityAgency/ghidra/releases/latest/" | grep "ghidra/releases/download" | cut -d '"' -f 2)
@@ -23,6 +23,8 @@ chisel_windows_url=$(curl -Ls "https://github.com/jpillora/chisel/releases/lates
 
 echo "[3/34] Downloading additionnals packages"
 apt-get install -y --allow-downgrades open-vm-tools open-vm-tools-desktop rdesktop zsh zsh-autosuggestions nfs-common default-mysql-client dnsenum libssl-dev libkrb5-dev libffi-dev python-dev-is-python3 build-essential jq exploitdb colortest ftp snmp hashid > /dev/null 2&>1
+
+git clone https://github.com/urbanadventurer/username-anarchy.git /opt/
 
 echo "[4/34] Downloading SecLists"
 wget -q https://github.com$secLists_url -O /tmp/tmp_downloads/SecLists.tar.gz
@@ -70,10 +72,10 @@ echo "[17/34] Downloading vscode"
 wget -q https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 -O /tmp/tmp_downloads/vscode.deb
 
 echo "[18/34] Downloading pwndbg"
-git clone https://github.com/pwndbg/pwndbg /home/parrot/pwndbg > /dev/null 2>&1
+git clone https://github.com/pwndbg/pwndbg /home/parrot/pwndbg 
 
 echo "[19/34] Downloading CrackMapExec"
-git clone https://github.com/Porchetta-Industries/CrackMapExec /home/parrot/CrackMapExec > /dev/null 2>&1
+git clone https://github.com/Porchetta-Industries/CrackMapExec /home/parrot/CrackMapExec 
 
 echo "[20/34] Downloading BurpSuite"
 wget -q "https://portswigger-cdn.net/burp/releases/download?type=Linux" -O /tmp/tmp_downloads/burpsuite.sh
@@ -89,7 +91,7 @@ mkdir -p /opt/wordlists/SecLists \
   && tar xf /tmp/tmp_downloads/SecLists.tar.gz -C /opt/wordlists/SecLists --strip-components 1
 
 echo "[24/34] Installing Ghidra"
-unzip /tmp/tmp_downloads/ghidra.zip -d /opt/ > /dev/null 2>&1 \
+unzip /tmp/tmp_downloads/ghidra.zip -d /opt/  \
   && mv /opt/ghidra_* /opt/Ghidra \
   && alias ghidra=/opt/Ghidra/ghidraRun
 
@@ -101,11 +103,11 @@ gzip -d /tmp/tmp_downloads/chisel_windows.gz \
   && mv /tmp/tmp_downloads/chisel_windows /opt/binaries/chisel_windows
 
 echo "[26/34] Installing sublime-text"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add - > /dev/null 2>&1 \
-  && apt-get -qq install apt-transport-https -y > /dev/null 2>&1 \
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -  \
+  && apt-get install apt-transport-https -y  \
   && echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list \
-  && apt-get -qq update -y > /dev/null 2>&1 \
-  && apt-get -qq install sublime-text -y > /dev/null 2>&1
+  && apt-get update -y  \
+  && apt-get install sublime-text -y 
 
 echo "[27/34] Installing vscode"
 dpkg -i /tmp/tmp_downloads/vscode.deb > /dev/null 2&>1
@@ -114,7 +116,7 @@ echo "[28/34] Installing CrackMapExec"
 cd /home/parrot/CrackMapExec; poetry install > /dev/null 2&>1; poetry run crackmapexec > /dev/null 2&>1
 
 echo "[29/34] Installing pwndbg"
-apt-get -qq autoremove --purge -y gdb > /dev/null 2&>1 \
+apt-get autoremove --purge -y gdb > /dev/null 2&>1 \
   && rm -rf /usr/share/gdb \
   && chown -R parrot:parrot /home/parrot/pwndbg \
   && sudo -H -u parrot /home/parrot/pwndbg/setup.sh > /dev/null 2&>1
@@ -125,7 +127,7 @@ chmod +x /tmp/tmp_downloads/oh-my-zsh_install.sh \
   && sudo -H -u parrot /tmp/tmp_downloads/oh-my-zsh_install.sh
 
 echo "[31/34] Downloading powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/parrot/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null 2>&1
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/parrot/.oh-my-zsh/custom}/themes/powerlevel10k 
 
 echo "[32/34] Installing powerlevel10k"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' /home/parrot/.zshrc \
@@ -140,5 +142,6 @@ chmod +x /tmp/tmp_downloads/burpsuite.sh \
 echo "[34/34] Finalizing installation"
 chown -R parrot:parrot /opt/
 chown -R parrot:parrot /home/parrot/
+echo "alias username-anarchy=/opt/username-anarchy/username-anarchy"
 
 echo "[!] Please reboot to make all changes applies !"
